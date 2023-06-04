40c0d2f7fb8c4671ec3add6c8e1b68c5b0527353
Gaurav Anand <gv.anand.1997@gmail.com>
2023-06-04 18:13:17 +0530

    Create House.java

---
+++ Creational/BuilderDesignPattern/House.java
@@ -0,0 +1,22 @@
+public class House {
+    String floorPlan;
+    String electrictyPlan;
+    String waterPlan;
+    String pillars;
+    String ceiling;
+    String tiles;
+    String plaster;
+    String paint;
+    String furniture;
+
+    void describeHouse(){
+        System.out.println("floorPlan = "+this.floorPlan);
+        System.out.println("electrictyPlan = "+this.electrictyPlan);
+        System.out.println("pillars = "+this.pillars);
+        System.out.println("ceiling = "+this.ceiling);
+        System.out.println("tiles = "+this.tiles);
+        System.out.println("plaster = "+this.plaster);
+        System.out.println("paint = "+this.paint);
+        System.out.println("furniture = "+this.furniture);
+    }
+}
